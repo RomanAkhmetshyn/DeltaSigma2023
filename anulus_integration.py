4bfd5b467f3a451a22ee508750e3ef732e5a03d7
RomanAkhmetshyn <roman_akhmetshyn@univ.kiev.ua>
2023-06-07 15:41:44 -0500

    pre optimized and new catalog

--- anulus_integration.py
+++ anulus_integration.py
@@ -31,20 +31,28 @@ cosmo = cosmology.setCosmology("737")
 
 #%%
 
-data = Table.read("./data/dr8_redmapper_v6.3.1_members_n_clusters_masked.fits")
+lenses = Table.read("./data/dr8_redmapper_v6.3.1_members_n_clusters_masked.fits")
 data_mask = (
-        (data["R"] >= 0.6)
-        & (data["R"] < 0.9)
-        & (data["ZSPEC"] > -1.0)
+        (lenses["R"] >= 0.6)
+        & (lenses["R"] < 0.9)
+        & (lenses["zspec"] > -1.0)
     )
-data = data[data_mask]
+lenses = lenses[data_mask]
 cdf_resolution=1000
-mass_per_point = 1098372.008822474
+mass_per_point = 1098372.008822474*5000
+start_bin=0.01
+end_bin=1.5
+ring_incr=0.02
+ring_num=round((end_bin-start_bin)/ring_incr)
+ring_radii = np.linspace(start_bin, end_bin, ring_num+1) * 1000
+threshold=ring_incr/2*100
 
 mdef="200m"
 
-for sat in data[0:1]:
-    
+DeltaSigmas=np.empty((0, len(ring_radii)))
+
+for sat in lenses[0:10]:
+    debug_start = time.time()
     c=concentration.concentration(
         M=sat["M_halo"], mdef="200m", z=sat["Z_halo"], model="duffy08"
     )
@@ -57,7 +65,7 @@ for sat in data[0:1]:
     #
     interp_radii = np.linspace(0, virial_radius, cdf_resolution)
     
-    debug_start = time.time()
+    # debug_start = time.time()
     # Temporarily ignore division by zero and overflow warnings
     with np.errstate(divide="ignore", over="ignore"):
         interp_delta_sigmas = halo_profile.deltaSigma(interp_radii)
@@ -69,10 +77,10 @@ for sat in data[0:1]:
         np.pi * interp_radii**2 * (interp_delta_sigmas + interp_surface_densities)
     )
 
-    print(
-        "Finished calculating enclosed mass with colossus after",
-        time.time() - debug_start,
-    )
+    # print(
+    #     "Finished calculating enclosed mass with colossus after",
+    #     time.time() - debug_start,
+    # )
     #
     # Determine number of points to generate for this halo
     #
@@ -82,19 +90,19 @@ for sat in data[0:1]:
     #
     # Make 1D interpolator for this halo
     #
-    print("Begin creating 2D NFW CDF interpolator")
-    debug_start2 = time.time()
+    # print("Begin creating 2D NFW CDF interpolator")
+    # debug_start2 = time.time()
     interp_normed_2d_encl_masses = interp1d(
         interp_2d_encl_masses / interp_2d_encl_masses[-1:][0],
         interp_radii,
         assume_sorted=True,
     )
 
-    print(
-        "Finished creating 2D NFW CDF interpolator after",
-        time.time() - debug_start2,
-    )
-    print()
+    # print(
+    #     "Finished creating 2D NFW CDF interpolator after",
+    #     time.time() - debug_start2,
+    # )
+    # print()
 
     #
     # Generate random points for this halo + offset combination
@@ -106,71 +114,50 @@ for sat in data[0:1]:
     offset_y = offset * np.sin(offset_angle)
     #
     random_cdf_yvals = rng.uniform(0, 1, size=n_points)
-    print("Begin interpolation")
-    debug_start3 = time.time()
+    # print("Begin interpolation")
+    # debug_start3 = time.time()
     random_radii = interp_normed_2d_encl_masses(random_cdf_yvals)
-    print("Finished interpolation after", time.time() - debug_start3)
+    # print("Finished interpolation after", time.time() - debug_start3)
     random_azimuths = rng.uniform(0, 2 * np.pi, size=n_points)
     random_radii_x = random_radii * np.cos(random_azimuths) + offset_x
     random_radii_y = random_radii * np.sin(random_azimuths) + offset_y
-    print("Begin extending list")
-    debug_start4 = time.time()
+    # print("Begin extending list")
+    # debug_start4 = time.time()
     #if return_xy:
 
     #else:
     random_r=np.array([ np.sqrt(random_radii_x**2 + random_radii_y**2)])
     random_theta=np.array([ np.arctan2(random_radii_y, random_radii_x)])
-    print("Finished extending list after", time.time() - debug_start4)
+    # print("Finished extending list after", time.time() - debug_start4)
     print()
 
-
-
-
-
-    # sat_x = sat['R'] * 100
-    # sat_y = 0
-    # ring_radii = np.linspace(0.1, 1.5, 14+1) * 10
     
-    # ring_counts = []
-    
-    # for radius in ring_radii:
-    #     count = 0
-    #     for x, y in zip(random_radii_x, random_radii_y):
-    #         distance = np.sqrt((x - sat_x)**2 + (y - sat_y)**2)
-    #         if radius - 0.5 <= distance <= radius + 0.5:
-    #             count += 1
-    #     ring_counts.append(count)
-    
-    # # Create a DataFrame using the ring radii and ring counts
-    # data = {'Ring Radii': ring_radii, 'Point Counts': ring_counts}
-    # df = pd.DataFrame(data)
-    
-    sat_x = sat['R'] * 100
+    sat_x = sat['R'] * 1000
     sat_y = 0
-    ring_radii = np.linspace(0.1, 1.5, 140+1) * 100
-    threshold=0.5
+    
     S=[np.pi*((r+threshold)**2-(r-threshold)**2) for r in ring_radii]
     # Calculate the distances for all points at once
     distances = np.sqrt((random_radii_x - sat_x)**2 + (random_radii_y - sat_y)**2)
     
     # Create an empty array to store the counts for each ring
-    ring_counts = np.zeros(len(ring_radii), dtype=int)
-    circle_counts = np.zeros(len(ring_radii), dtype=int)
+    ring_counts = np.zeros(len(ring_radii), dtype=np.int64)
+    circle_counts = np.zeros(len(ring_radii), dtype=np.int64)
     
     # Iterate over each ring radius and count the points within each ring
     for i in range(len(ring_radii)):
         mask = np.logical_and(ring_radii[i] - threshold <= distances, distances <= ring_radii[i] + threshold)
         # ring_counts[i] = np.sum(mask)
+        
         ring_counts[i] = np.sum(mask)*mass_per_point/S[i]
         
     for i in range(len(ring_radii)):
         mask = np.logical_and(0 <= distances, distances <= ring_radii[i] - threshold)
         # ring_counts[i] = np.sum(mask)
-        circle_counts[i] = np.sum(mask)*mass_per_point/S[i]
+        circle_counts[i] = np.sum(mask)*mass_per_point/(np.pi*(ring_radii[i]- threshold)**2)
 
     # Create a DataFrame using the ring radii and ring counts
     data = {'Ring Radii': ring_radii, 'Delta(R)': ring_counts}
-    df = pd.DataFrame(data)
+    # df = pd.DataFrame(data)
     
     sums=[]
 
@@ -178,8 +165,43 @@ for sat in data[0:1]:
         DeltalessR=circle_counts[i]
         DeltaR=ring_counts[i]
         sums.append(DeltalessR-DeltaR)
+    data2 = {'Ring Radii': ring_radii[::-1], 'SigmaDelta(R)': sums}
+    t=time.time() - debug_start
+    print(
+        "Finished calculating 1 sat after",
+        t,
+    )
+    DeltaSigmas=np.append(DeltaSigmas, [np.array(sums)],axis=0)
+    
+    # fig, axes = plt.subplots(nrows=2, ncols=1)
 
-        
+    # # Plot the first graph on the left subplot
+    # axes[0].plot(data['Ring Radii'],data['Delta(R)'])
+    # axes[0].set_xlabel('kpc')
+    # axes[0].set_ylabel('sigma(R)')
+
+    
+    # # Plot the second graph on the right subplot
+    # axes[1].plot(data2['Ring Radii'],data2['SigmaDelta(R)'])
+    # axes[1].set_xlabel('kpc')
+    # axes[1].set_ylabel('DeltaSigma')
+
+    # fig.suptitle('mpp=%.2e, n=%.3e, t=%.2f, rings=%i'%(mass_per_point, n_points,  t, len(ring_radii)))
+
+    # plt.show()
+
+
+avgDsigma=np.mean(DeltaSigmas,axis=0)
+
+fig, axes = plt.subplots(nrows=1, ncols=1)
+
+# Plot the second graph on the right subplot
+axes.plot(data2['Ring Radii'],avgDsigma)
+axes.set_xlabel('kpc')
+axes.set_ylabel('avg DeltaSigma')
+
+
+plt.show()
     
 
         
@@ -200,34 +222,5 @@ for sat in data[0:1]:
     #     plt.show()
                 
         
-    # with plt.rc_context({"axes.grid": False}):
-    #     fig, ax = plt.subplots(dpi=100)
-    #     img = ax.hexbin(random_radii_x, random_radii_y, gridsize=100, bins="log")
-    #     ax.plot(0, 0, "r+")
-    #     fig.colorbar(img)
-    #     ax.scatter(x_coords, y_coords, s=1, c='yellow',zorder=5)
-    #     ax.set_aspect("equal")
-    #     ax.set_title("1 halos")
-    #     plt.show()
-    
-
-    
-    # distances = np.sqrt((random_radii_x - sat_x)**2 + (random_radii_y - sat_y)**2)
-    # ring_radii = np.linspace(0.1, 1.5, 14+1)
-    # tolerance = 0.05  # Example tolerance threshold
-    # ring_counts = []
-    
-    # for i in range(len(ring_radii)):
-    #     # Calculate the lower and upper boundaries of the tolerance range
-    #     lower_bound = ring_radii[i] - tolerance
-    #     upper_bound = ring_radii[i] + tolerance
-    
-    #     # Count the number of points within the tolerance range for the current ring
-    #     count_within_tolerance = np.sum(
-    #         (distances >= lower_bound) & (distances < upper_bound)
-    #     )
-        
-    #     # Add the count to the ring_counts list
-    #     ring_counts.append(count_within_tolerance)
 
 
--- sats_n_clusters.py
+++ sats_n_clusters.py
@@ -12,31 +12,39 @@ from astropy.table import Table, vstack, Column
 import math
 
 
-sats = Table.read("D:/GitHub/summer-research/data/dr8_redmapper_v6.3.1_members_masked.fits")
-clusters= Table.read("D:/GitHub/summer-research/data/dr8_run_redmapper_v6.3.1_lgt20_catalog.fit")
+sats = Table.read("D:/GitHub/summer-research/data/remapper_cat+mem/dr8_members.fit")
+clusters= Table.read("D:/GitHub/summer-research/data/remapper_cat+mem/dr8_cat.fit")
 
 cluster_masses=[]
 cluster_z=[]
 
-IDs=[]
+cluster_dic={}
 
-for match_id in sats['MEM_MATCH_ID']:
+for match_id in sats['ID']:
     print(match_id)
-    IDs.append(match_id)
+    if match_id not in cluster_dic:
         
-    for i, value in enumerate(clusters['MEM_MATCH_ID']):
-        if value == match_id:
-            row_number = i
-            break  # Exit the loop once the value is found
+        
+        for i, value in enumerate(clusters['ID']):
+            if value == match_id:
+                row_number = i
+                break  # Exit the loop once the value is found
+        
+        z=clusters['zlambda'][row_number]
+        cluster_z.append(z)
+        richness=clusters['lambda'][row_number]
+        mass=math.exp(1.48)*(richness/60)**(1.06)*10**(14)
+        cluster_masses.append(mass)
+        cluster_dic[match_id]=[mass,z]
+        
+    elif match_id in cluster_dic:
+        cluster_z.append(cluster_dic[match_id][1])
+        cluster_masses.append(cluster_dic[match_id][0])
     
-    cluster_z.append(clusters['ZRED'][row_number])
-    richness=clusters['Z_LAMBDA'][row_number]
-    mass=math.exp(1.48)*(richness/60)**(1.06)*10**(14)
-    cluster_masses.append(mass)
-    
+print('creating columns')
 mass_column=Column(cluster_masses, name='M_halo')
 redshift_column=Column(cluster_z, name='Z_halo')
-
+print('adding columns')
 sats.add_column(mass_column)
 sats.add_column(redshift_column)
 sats.write('D:/GitHub/summer-research/data/dr8_redmapper_v6.3.1_members_n_clusters_masked.fits', overwrite=True)  # Replace 'modified_table.fits' with the desired file name or path
